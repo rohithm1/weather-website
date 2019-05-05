5e1c81fc024b265ba607ee4bdda2955799e9d512
Rohith Mandavilli <Rohith.Mandavilli.22@Dartmouth.edu>
2019-05-05 18:11:18 -0400

    Website changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
-console.log('starting up!');
+// change require to es6 import style
+import './style.scss';
+import * as $ from 'jquery';
+
+// instantiate needed keys and urls to get later API calls
+const apiKey2 = 'd965e8bc344b511c07399993f53aa93f';
+const openweather = 'https://api.openweathermap.org/data/2.5/forecast?lat=';
+const apiKey1 = 'f3ef0dfc1ddca61f3fa5d8ac1feff375';
+const darksky = 'https://api.darksky.net/forecast/';
+
+const location = $('#location');
+
+// if the call was successful
+function success(position) {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+
+  // API call
+  $.getJSON(
+    `${darksky + apiKey1}/${latitude},${longitude}?callback=?`,
+    (data) => {
+      // grabs the termp and minutely data from darksky
+      $('#temp').html(`${data.currently.temperature}° F`);
+      $('#minutely').html(data.minutely.summary);
+      $('#current').html('Current Overview');
+      $('#location').html(`Latitude is ${latitude}° Longitude is ${longitude}°`);
+    },
+  );
+
+  $('#forecast').html('Next Week Forecasted Weather');
+
+  // API call for the forecasted weather - from openweather data
+  $.ajax({
+    url: `${openweather + latitude}&lon=${longitude}`, // API Call
+    dataType: 'json',
+    type: 'GET',
+    data: {
+      appid: apiKey2,
+      units: 'imperial', // fahrenheit
+      cnt: '7', // 7 days
+    },
+    success(data) {
+      const loc = `You are in ${data.city.name}`;
+      $('#loc').html(`${loc}!`);
+      // for each day
+      $.each(data.list, (index, val) => {
+        const day = `Day ${index + 1}: `; // Day
+        $(`#day${index + 1}`).html(day);
+        const temp = `${val.main.temp}&degF`; // Temperature
+        $(`#temp${index + 1}`).html(temp);
+        const description = `<span> ${val.weather[0].description}</span>`; // Description
+        $(`#description${index + 1}`).html(description);
+        const image = `<img src='https://openweathermap.org/img/w/${val.weather[0].icon}.png'>`; // Icon
+        $(`#pic${index + 1}`).html(image);
+      });
+    },
+  });
+}
+
+// if it didnt work
+function error() {
+  location.innerHTML = 'Unable to retrieve your location';
+}
+
+// gets the current lat lon og the device
+navigator.geolocation.getCurrentPosition(success, error);
+
+location.innerHTML = 'Locating...';
--- temporary/weather.js
+++ temporary/weather.js
@@ -1,75 +1,76 @@
-function Weather() {
-  //instantiate needed keys and urls to get later API calls
-  var apiKey2 = "d965e8bc344b511c07399993f53aa93f";
-  var openweather = "https://api.openweathermap.org/data/2.5/forecast?lat=";
-  var location = document.getElementById("location");
-  var apiKey1 = "f3ef0dfc1ddca61f3fa5d8ac1feff375";
-  var darksky = "https://api.darksky.net/forecast/";
-  var apiKey2 = "d965e8bc344b511c07399993f53aa93f";
-  //gets the current lat lon og the device
-  navigator.geolocation.getCurrentPosition(success, error);
+import * as $ from 'jquery';
 
-  //if the call was successful
+function weather() {
+  // instantiate needed keys and urls to get later API calls
+  const apiKey2 = 'd965e8bc344b511c07399993f53aa93f';
+  const openweather = 'https://api.openweathermap.org/data/2.5/forecast?lat=';
+  const location = document.getElementById('location');
+  const apiKey1 = 'f3ef0dfc1ddca61f3fa5d8ac1feff375';
+  const darksky = 'https://api.darksky.net/forecast/';
+
+  // if the call was successful
   function success(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
 
-    //print out where the user is
-    current.innerHTML =
-      "Current Overview";
-    location.innerHTML =
-      "Latitude is " + latitude + "° Longitude is " + longitude + "°";
+    const current = $('#current');
+    const forecast = $('#forecast');
+
+    // print out where the user is
+    current.innerHTML = 'Current Overview';
+    location.innerHTML = `Latitude is ${latitude}° Longitude is ${longitude}°`;
 
 
-    //API call
+    // API call
     $.getJSON(
-      darksky + apiKey1 + "/" + latitude + "," + longitude + "?callback=?",
-      function(data) {
-        //grabs the termp and minutely data from darksky
-        $("#temp").html(data.currently.temperature + "° F");
-        $("#minutely").html(data.minutely.summary);
-      }
+      `${darksky + apiKey1}/${latitude},${longitude}?callback=?`,
+      (data) => {
+        // grabs the termp and minutely data from darksky
+        $('#temp').html(`${data.currently.temperature}° F`);
+        $('#minutely').html(data.minutely.summary);
+      },
     );
-    forecast.innerHTML =
-      "Next Week Forecasted Weather";
 
+    forecast.innerHTML = 'Next Week Forecasted Weather';
 
-    //API call for the forecasted weather - from openweather data
+    // API call for the forecasted weather - from openweather data
     $.ajax({
-      url: openweather + latitude + "&lon=" + longitude, //API Call
-      dataType: "json",
-      type: "GET",
+      url: `${openweather + latitude}&lon=${longitude}`, // API Call
+      dataType: 'json',
+      type: 'GET',
       data: {
         appid: apiKey2,
-        units: "imperial", //fahrenheit
-        cnt: "7" //7 days
+        units: 'imperial', // fahrenheit
+        cnt: '7', // 7 days
       },
-        success: function(data) {
-          loc = "<h2>" + "You are in " + data.city.name + "! </h2>";
-          $("#loc").html(loc);
+      success(data) {
+        const loc = `${'<h2>' + 'You are in '}${data.city.name}! </h2>`;
+        $('#loc').html(loc);
 
-          //for each day
-          $.each(data.list, function(index, val) {
-            index = index + 1;
-            var day = "Day " + index + ": " // Day
-            $("#day" + index).html(day);
-            var temp = val.main.temp + "&degF" // Temperature
-            $("#temp" + index).html(temp);
-            var description = "<span> " + val.weather[0].description + "</span>"; // Description
-            $("#description" + index).html(description);
-            var image = "<img src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'>" // Icon
-            $("#pic" + index).html(image);
-          });
-        }
-      });
+        // for each day
+        $.each(data.list, (index, val) => {
+          const day = `Day ${index + 1}: `; // Day
+          $(`#day${index + 1}`).html(day);
+          const temp = `${val.main.temp}&degF`; // Temperature
+          $(`#temp${index + 1}`).html(temp);
+          const description = `<span> ${val.weather[0].description}</span>`; // Description
+          $(`#description${index + 1}`).html(description);
+          const image = `<img src='https://openweathermap.org/img/w/${val.weather[0].icon}.png'>`; // Icon
+          $(`#pic${index + 1}`).html(image);
+        });
+      },
+    });
   }
-  
-  //if it didnt work
+
+  // if it didnt work
   function error() {
-    location.innerHTML = "Unable to retrieve your location";
+    location.innerHTML = 'Unable to retrieve your location';
   }
 
+  // gets the current lat lon og the device
+  navigator.geolocation.getCurrentPosition(success, error);
 
-  location.innerHTML = "Locating...";
+  location.innerHTML = 'Locating...';
 }
-Weather();
+
+weather();
